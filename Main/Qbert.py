cd66b5cb9fecf6b14bd00ead792ab25dceeaee99
Supersonic187 <giannicarreon@gmail.com>
2025-03-24 14:21:54 -0500

    Update Qbert.py
    added escape button closing game

--- Main/Qbert.py
+++ Main/Qbert.py
@@ -1,17 +1,26 @@
 import pygame
-
+import sys
+import random
+import time
 pygame.init()
 
+# Screen
 screen_width = 800
 screen_height = 600
-
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Qbrt")
 
 run = True
 while run:
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-          run = False
+            run = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+    # Update display lmfao
+    pygame.display.flip()
 
 pygame.quit()
+sys.exit()
